c3020dcb930d57c8c8006db8efd139d4145b4317
AbhijayBisht <47766010+AbhijayBisht@users.noreply.github.com>
2021-07-11 11:59:14 +0530

    rev 0.1
    Initial commit to create repository

---
+++ bfl-config-server/src/test/java/com/studyforlife/configapi/BflConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyforlife.configapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BflConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
